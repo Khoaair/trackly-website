208d4412b264fd6c4d2ea0f6b7c060d0b45eb978
KhoaAir win <KhoaAir@gmail.com>
2023-03-12 13:13:21 +0700

    first trackly website

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProductPage,
+} from './pages';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='cart' element={<Cart />} />
+        <Route path='products' element={<Products />} />
+        <Route path='products/:id' element={<SingleProductPage />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
